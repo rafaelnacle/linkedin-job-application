3acaacfb18234bd2d8c07199e85b0d1cb42969b2
= <=>
2025-04-04 16:36:53 -0300

    apply to simple application

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ load_dotenv()
 
 ACCOUNT_EMAIL = os.getenv("ACCOUNT_EMAIL")
 ACCOUNT_PASSWORD = os.getenv("ACCOUNT_PASSWORD")
+PHONE = "999999999"
+YEARS_OF_XP = "1"
 
 # Keep the browser open
 chrome_options = webdriver.ChromeOptions()
@@ -45,3 +47,34 @@ password_field.send_keys(Keys.ENTER)
 
 # If captcha is presented - solve manually
 # input("Press Enter when you have solved the Captcha")
+
+# Locate apply button
+time.sleep(5)
+apply_button = driver.find_element(By.CSS_SELECTOR, value=".jobs-s-apply button")
+apply_button.click()
+
+# If the application requires phone number and the field is empty, then fill in the number
+time.sleep(5)
+phone = driver.find_element(By.XPATH, value='//*[@id="single-line-text-form-component-formElement-urn-li-jobs-applyformcommon-easyApplyFormElement-4195448559-10862781777-phoneNumber-nationalNumber"]')
+if phone.text == "":
+    phone.send_keys(PHONE)
+
+time.sleep(2)
+# Next step 1
+submit_button = driver.find_element(By.CSS_SELECTOR, value="footer button")
+submit_button.click()
+
+# Next step 2
+time.sleep(2)
+submit_button.click()
+
+years_of_xp = driver.find_element(By.CSS_SELECTOR, value="#ember402 input")
+if years_of_xp.get_attribute("value") == "":
+    years_of_xp.send_keys(YEARS_OF_XP)
+
+time.sleep(2)
+revision_btn = driver.find_element(By.ID, value="ember403")
+revision_btn.click()
+
+# Send CV Button
+send_cv_btn = driver.find_element(By.ID, value="ember414")
